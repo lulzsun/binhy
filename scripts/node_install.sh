b04a60fc20eb1e4faac188a9318339d8beefdf91
Jimmy Quach <jminquach@gmail.com>
2023-09-13 19:02:25 -0700

    fix node.js and npm (again)

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-NODE_MAJOR=18
-echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update
-sudo apt install nodejs
-sudo apt install build-essential
+wget https://unofficial-builds.nodejs.org/download/release/v18.12.0/node-v18.12.0-linux-armv6l.tar.gz
+tar xvfJ node-v18.12.0-linux-armv6l.tar.gz
+sudo cp -R node-v18.12.0-linux-armv6l/* /usr/local
+rm -rf node-v18.12.0-*
+PATH=$PATH:/usr/local/bin
