a959b7b007b93846db3cf110e3b21218e1e148b1
Jimmy Quach <jminquach@gmail.com>
2023-09-13 19:26:36 -0700

    update nodejs to v18.14.2

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://unofficial-builds.nodejs.org/download/release/v18.12.0/node-v18.12.0-linux-armv6l.tar.gz
-tar -xzf node-v18.12.0-linux-armv6l.tar.gz
-sudo cp -R node-v18.12.0-linux-armv6l/* /usr/local
-rm -rf node-v18.12.0-*
+wget https://unofficial-builds.nodejs.org/download/release/v18.14.2/node-v18.14.2-linux-armv6l.tar.gz
+tar -xzf node-v18.14.2-linux-armv6l.tar.gz
+sudo cp -R node-v18.14.2-linux-armv6l/* /usr/local
+rm -rf node-v18.14.2-*
 PATH=$PATH:/usr/local/bin
