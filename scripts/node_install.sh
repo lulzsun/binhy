231ab2d3e7a2d90e3e539726fcfd45f666ac6fcc
Jimmy Quach <jminquach@gmail.com>
2023-09-13 19:13:43 -0700

    fix using wrong tar file format

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://unofficial-builds.nodejs.org/download/release/v18.12.0/node-v18.12.0-linux-armv6l.tar.gz
-tar xvfJ node-v18.12.0-linux-armv6l.tar.gz
+tar -xzf node-v18.12.0-linux-armv6l.tar.gz
 sudo cp -R node-v18.12.0-linux-armv6l/* /usr/local
 rm -rf node-v18.12.0-*
 PATH=$PATH:/usr/local/bin
