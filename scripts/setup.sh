fdf4ed60ff1a0f9bd0091d7f65938febc80371be
Jimmy Quach <jminquach@gmail.com>
2023-10-08 17:54:49 -0700

    add systemd service

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,3 +11,10 @@ chmod +x ./node_install.sh
 ./node_install.sh
 
 source ~/.bashrc
+
+cp ./binhy.service /etc/systemd/system/
+
+systemctl daemon-reload
+
+systemctl enable binhy
+systemctl start binhy
